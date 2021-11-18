6c79868f03a61dfa5c5d6c2945a93f899f7b0620
NickSagan <9952728@gmail.com>
2021-11-18 19:37:03 +0300

    Initial Commit

---
+++ Detect-a-Beacon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Detect-a-Beacon
+//
+//  Created by Nick Sagan on 18.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
