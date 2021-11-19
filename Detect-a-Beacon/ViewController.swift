d80be9448669f64def0e22a2e53611420b3b2149
NickSagan <9952728@gmail.com>
2021-11-19 16:29:42 +0300

    Add func update(distance: CLProximity) add startscaner()

--- Detect-a-Beacon/ViewController.swift
+++ Detect-a-Beacon/ViewController.swift
@@ -26,11 +26,47 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         if status == .authorizedAlways {
             if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
                 if CLLocationManager.isRangingAvailable() {
-                    // do stuff
+                    
+//we only start scanning for beacons when we have permission and if the device is able to do so
+                    
+                    startScanning()
                 }
             }
         }
     }
+    
+    func startScanning() {
+        let uuid = UUID(uuidString: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5")!
+        let beaconRegion = CLBeaconRegion(proximityUUID: uuid, major: 123, minor: 456, identifier: "MyBeacon")
 
+        locationManager?.startMonitoring(for: beaconRegion)
+        locationManager?.startRangingBeacons(in: beaconRegion)
+    }
+    
+    func update(distance: CLProximity) {
+        UIView.animate(withDuration: 1) {
+            switch distance {
+            case .unknown:
+                self.view.backgroundColor = UIColor.gray
+                self.distanceReader.text = "UNKNOWN"
+
+            case .far:
+                self.view.backgroundColor = UIColor.blue
+                self.distanceRdistanceReadereading.text = "FAR"
+
+            case .near:
+                self.view.backgroundColor = UIColor.orange
+                self.distanceReader.text = "NEAR"
+
+            case .immediate:
+                self.view.backgroundColor = UIColor.red
+                self.distanceReader.text = "RIGHT HERE"
+                
+            @unknown default:
+                self.view.backgroundColor = .black
+                self.distanceReader.text = "WHOA!"
+            }
+        }
+    }
 }
 
